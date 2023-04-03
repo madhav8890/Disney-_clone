f825ab1692faf6add75090a978355ed2c628fd38
Madhav Prajapati <madhavku248@gmail.com>
2023-04-03 23:50:06 +0530

    setup the login page

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<Login/>}/>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
